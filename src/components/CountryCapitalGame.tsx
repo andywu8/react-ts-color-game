9bc09996ab17ab3dc11731aa4aec97c2652428ab
Andy Wu <andyww09@gmail.com>
2023-10-31 11:46:44 -0700

    move countrycapitalgame to components

---
+++ src/components/CountryCapitalGame.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+type ButtonState = 'DEFAULT' | 'SELECTED' | 'WRONG';
+type Option = {
+  value: string,
+  state: ButtonState;
+}
+function CountryCapitalGame({data}: {data: Record<string, string>}){
+    const countries = Object.keys(data);
+    const capitals = Object.values(data);
+    const [colorMap, setColorMap] = useState<Record<string, string>>({});
+    const [options, setOptions] = useState<Option[]>(
+      [...countries, ...capitals].map((value) => ({
+        value,
+        state: 'DEFAULT'
+      })).sort(() => Math.random() - 0.5) as Option[]
+      );
+  
+    return (
+      <>
+        {options.map((option) => (
+          <button
+          className={option.state === 'SELECTED' ? 'selected' : ''}
+          onClick={() => {
+            setOptions(
+              options.map((opt) => (
+                opt === option ? {
+                  ...opt,
+                  state: 'SELECTED'
+                } : opt
+              ))
+            );
+            // set bg to blue 4009Bff
+          }}
+          >{option.value}</button>
+        ))}
+      </>
+    )
+  }
+export default CountryCapitalGame;
